2a3c51bb2805fbd933fd64a7b15ebab94f9c465e
Zuri97 <christopher@figaredo.net>
2018-11-28 18:34:43 -0600

    Gulp SASS without reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    mincss = require('gulp-cssnano');
+
+var sassOptions = {
+    outputStyle: 'expanded'
+};
+      
+var prefixerOptions = {
+    browsers: ['last 2 versions']
+};
 
 gulp.task('sass', function(){
 
-    return gulp.src('./page/sass/estilos.scss').pipe(sass()).pipe(gulp.dest('./page/css'));
+    return gulp.src('./page/sass/main.scss')
+        .pipe(sass(sassOptions))
+        .pipe(autoprefixer(prefixerOptions))
+        .pipe(rename('estilos.css'))
+        .pipe(gulp.dest('./page/css'))
+        .pipe(mincss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./page/css'));
 
 });
 
