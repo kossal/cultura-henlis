39623621ec955a8efaa37f7a61f72528f90a26d0
Zuri97 <christopher@figaredo.net>
2018-12-04 01:53:00 -0600

    Gulp installed and deploy to another host

---
+++ gulp/tasks/dist.babel.js
@@ -0,0 +1,25 @@
+import {src, dest, series, parallel} from 'gulp';
+import {styles} from './styles.babel';
+import newer from 'gulp-newer';
+
+const srcDir = [
+    './src/**/*.html',
+    './src/css/styles.min.css',
+    './src/css/fonts/**/*',
+    '!./src/css/fonts/**/*.css',
+    './src/img/**/*',
+    './src/favicon.ico'
+];
+
+const srcToDist = () => {
+
+    return src(srcDir, {base: './src'})
+        .pipe(newer('./dist'))
+        .pipe(dest('./dist'));
+
+};
+
+const dist = series(styles, srcToDist);
+
+
+export {dist};
